6c603fb171fd594e4ceb190b1622994b80a721c2
begumbeyza <begumbeyza98@gmail.com>
2022-04-15 14:07:51 +0300

    Test

---
+++ test/src/main/java/com/sensai/test/SumFunct.java
@@ -0,0 +1,11 @@
+package com.sensai.test;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public  class SumFunct {
+    public static  void simplesum(Context context,String msg){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+
+    }
+}
